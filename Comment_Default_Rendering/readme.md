0db86beb0ff2c7ea1783873fb8307421ea94ff57
tommy chan <tommy.chan@adelphigroup.com>
2024-05-10 15:24:36 +0100

    update on question set up

--- Comment_Default_Rendering/readme.md
+++
@@ -1,6 +0,0 @@
-CustomQuestion with nodejs pipeline support.
-
-Commands:
-npm install
-npm start
-npm run build
--- Comment_Default_Rendering/runtime/loader.js
+++
@@ -1,5 +0,0 @@
-import render from './component';
-
-(function () {
-  Confirmit.pageView.registerCustomQuestion('9e023c0f-0efe-4e65-b201-b8cda3ec40b7', render);
-})();
